e6243438e8b1e16b3a7dc537a329538234522dc0
Éric Spérano <eric.sperano@gmail.com>
2025-09-03 12:03:22 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::{PathBuf};
 use anyhow::{Result};
@@ -8,6 +8,9 @@ mod cache;
 mod csv;
 mod generator;
 mod model;
+mod utils;
+
+use utils::pluralize as plrze;
 
 #[derive(Parser)]
 #[command(name = "briq-utils")]
@@ -30,6 +33,8 @@ enum Commands {
     Analyze {
         #[arg(short, long)]
         workdir: String,
+        #[arg(short, long)]
+        set: Option<String>
     },
     Mirror {
         #[arg(short, long)]
@@ -37,128 +42,6 @@ enum Commands {
         #[arg(short, long)]
         workdir: String,
     }
-}
-
-fn part_csv_to_model(part: csv::PartRecord) -> model::Part {
-    model::Part {
-        number: part.part_num,
-        name: part.name,
-        part_category_id: part.part_cat_id,
-        material: part.part_material,
-    }
-}
-
-fn minifig_csv_to_model(minifig: csv::MinifigRecord) -> model::Minifig {
-    model::Minifig {
-        number: minifig.fig_num,
-        name: minifig.name,
-        parts_count: minifig.num_parts,
-        img_url: convert_asset_url(&minifig.img_url),
-    }
-}
-
-fn set_csv_to_model(set: csv::SetRecord, is_pack: bool, is_unreleased: bool, is_accessories: bool) -> model::Set {
-    model::Set {
-        number: set.set_num,
-        name: set.name,
-        year: set.year,
-        parts_count: set.num_parts,
-        theme_id: set.theme_id,
-        img_url: convert_asset_url(&set.img_url),
-        versions: vec![],
-        is_pack,
-        is_unreleased,
-        is_accessories,
-    }
-}
-
-fn convert_asset_url(url: &str) -> Option<String> {
-    if !url.is_empty() {
-        static PREFIX: &str = "https://cdn.rebrickable.com/media";
-        return Some(format!("https://briq-assets.spe.quebec{}", &url[PREFIX.len()..]));
-    }
-    None
-}
-
-fn get_set_version(inventory_id: u32, version: u16, minifig_inventories: &HashMap<u32, Vec<csv::InventoryMinifigRecord>>, part_inventories: &HashMap<u32, Vec<csv::InventoryPartRecord>>, all_parts_keys: &HashMap<String, bool>) -> model::SetVersion {
-    let mut version = model::SetVersion {
-        version,
-        minifigs: vec![],
-        parts: vec![],
-    };
-
-    if let Some(minifigs) = minifig_inventories.get(&inventory_id) {
-        for minifig in minifigs {
-            let minifig = model::SetMinifig {
-                number: minifig.fig_num.clone(), // TODO no clone
-                quantity: minifig.quantity,
-            };
-            version.minifigs.push(minifig);
-        } 
-    }
-    if let Some(parts) = part_inventories.get(&inventory_id) {
-        for part in parts {
-            if all_parts_keys.contains_key(&part.part_num) {
-                let part = model::SetPart {
-                    number: part.part_num.clone(), // TODO no clone
-                    quantity: part.quantity,
-                    color_id: part.color_id.try_into().unwrap(),
-                    img_url: convert_asset_url(&part.img_url),
-                    is_spare: part.is_spare == "True",
-                };
-                version.parts.push(part);
-            } else {
-                eprintln!("Set Version {}: Ignoring part {}: does not exist", version.version, part.part_num);
-            }
-        }
-    }
-    version
-}
-
-fn convert_to_model(csv_data: csv::Data) -> Box<model::Data> {
-    let mut parts: Vec<model::Part> = Vec::with_capacity(csv_data.parts.len());
-    let mut parts_map: HashMap<String, bool> = HashMap::new();
-    for part in csv_data.parts.into_iter() {
-        parts_map.insert(part.part_num.clone(), true);
-        parts.push(part_csv_to_model(part));
-    }
-    let mut minifigs: Vec<model::Minifig> = Vec::with_capacity(csv_data.minifigs.len());
-    for minifig in csv_data.minifigs.into_iter() {
-        minifigs.push(minifig_csv_to_model(minifig));
-    }
-    let mut set_inventories: HashMap<String, Vec<(u32, u16)>> = HashMap::new(); 
-    for inventory in csv_data.inventories.into_iter() {
-        let versions = set_inventories.entry(inventory.set_num.clone()).or_default();
-        versions.push((inventory.id, inventory.version));        
-    }
-    let mut part_inventories: HashMap<u32, Vec<csv::InventoryPartRecord>> = HashMap::new();
-    for part in csv_data.inventories_parts.into_iter() {
-        let parts = part_inventories.entry(part.inventory_id).or_default(); 
-        parts.push(part);
-    }
-    let mut minifig_inventories: HashMap<u32, Vec<csv::InventoryMinifigRecord>> = HashMap::new();
-    for minifig in csv_data.inventories_minifigs.into_iter() {
-        let minifigs = minifig_inventories.entry(minifig.inventory_id).or_default();
-        minifigs.push(minifig);
-    }
-    let mut sets: Vec<model::Set> = Vec::with_capacity(csv_data.sets.len());
-    for set in csv_data.sets.into_iter() {
-        let key = set.set_num.clone();
-        let mut set = set_csv_to_model(set, model::is_pack(&key), model::is_unreleased(&key), model::is_accessories(&key));
-        if let Some(versions) = set_inventories.get(&set.number) {
-            for version in versions {
-                let version = get_set_version(version.0, version.1, &minifig_inventories, &part_inventories, &parts_map);
-                set.versions.push(version);
-            }
-        }
-        sets.push(set);
-    }
-    Box::new(model::Data{
-        minifigs,
-        parts,
-        sets,
-        //themes,
-    })
 }
 
 fn main() -> Result<()> {
@@ -178,7 +61,10 @@ fn main() -> Result<()> {
                     let themes = generator::themes(&data.themes);
                     fs::write(workdir.join("Themes.swift"), themes)?;
                     println!("Converting data to BRIQ model...");
-                    let data = convert_to_model(*data);
+                    let data = model::convert(*data);
+                    println!("Generating more Swift code...");
+                    let sets = generator::sets(&data.sets);
+                    fs::write(workdir.join("Sets.swift"), sets)?;
                     println!("Generating JSON...");
                     let json_string = serde_json::to_string_pretty(&data)?;
                     fs::write(workdir.join("init.json"), json_string)?;
@@ -205,13 +91,17 @@ fn main() -> Result<()> {
             }
             Ok(())
         },
-        Commands::Analyze { workdir } => {
+        Commands::Analyze { workdir, set } => {
             println!("Reading all CSV data...");
             match csv::read_all(workdir) {
                 Ok(data) => {
                     println!("Themes tree has a max depth of {}", get_themes_tree_depth(&data.themes));
+                    let materials = data.parts.iter().map(|p|&p.part_material);
+                    let materials: HashSet<_> = materials.into_iter().collect();
+                    println!("There are {} unique parts materials.", materials.len());
+
                     println!("Converting data to BRIQ model...");
-                    let data = convert_to_model(*data);
+                    let data = model::convert(*data);
                     println!("Analyzing data...");
                     let mut count = 0;
                     let mut count2 = 0;
@@ -225,12 +115,42 @@ fn main() -> Result<()> {
                     }
                     let avg = ((count as f32) / (data.sets.len() as f32)) * 100.0;
                     let avg2 = ((count2 as f32) / (data.sets.len() as f32)) * 100.0;
-                    println!("{} sets has more than 1 version ({:.1}% of sets). {} sets has more than 2 versions ({:.1}%).", count, avg+0.5, count2, avg2+0.5);
+                    println!("{} has more than 1 version ({:.1}% of sets). {} has more than 2 versions ({:.1}%).", 
+                        plrze(count, "set"), avg+0.5, plrze(count2, "set"), avg2+0.5);
+                    if let Some(set) = set {
+                        let number = set;
+                        let set = data.sets.iter().find(|&s| s.number == *set);
+                        if let Some(set) = set {
+                            println!("Set {} has {}:", set.number, plrze(set.versions.len(), "version"));
+                            let mut all_parts: Vec<Vec<model::SetPart>> = Vec::new();
+                            for (i, version) in set.versions.iter().enumerate() {
+                                println!("Version #{}: {}, {}.", i, plrze(version.minifigs.len(), "minifig"), plrze(version.parts.len(), "part"));
+                                all_parts.push(version.parts.clone());
+                                for m in &version.minifigs {
+                                    println!("- {:?}", m);
+                                }
+                            }
+                            let mut parts = process(all_parts.clone());
+                            let common = parts.pop().unwrap();
+                            println!("Parts common to all versions:");
+                            for p in &common {
+                                println!("- {:?}", p);
+                            }
+                            for (i, parts) in parts.iter().enumerate() {
+                                println!("Parts of version {i}:");
+                                for p in parts {
+                                    println!("- {:?}", p);
+                                }
+                            }
+                        } else {
+                            eprintln!("Invalid set: {}", number)
+                        }
+                    }
                 }
                 Err(err) => {
                     eprintln!("{}", err);
                     return Err(err)
-                }
+}
             }
             Ok(())
         },
@@ -291,3 +211,40 @@ fn get_themes_tree_depth(themes: &[csv::ThemeRecord]) -> u32 {
     };
     max
 }
+
+fn intersection_all(inputs: &[Vec<model::SetPart>]) -> HashSet<model::SetPart> {
+    if inputs.is_empty() {
+        return HashSet::new();
+    }
+
+    let mut iter = inputs.iter();
+    let first: HashSet<_> = iter.next().unwrap().iter().cloned().collect();
+
+    iter.fold(first, |acc, set| {
+        acc.intersection(&set.iter().cloned().collect())
+            .cloned()
+            .collect()
+    })
+}
+
+fn unique_parts(input: &[model::SetPart], intersection: &HashSet<model::SetPart>) -> Vec<model::SetPart> {
+    input
+        .iter()
+        .filter(|&p| !intersection.contains(p))
+        .cloned()
+        .collect()
+}
+
+fn process(inputs: Vec<Vec<model::SetPart>>) -> Vec<Vec<model::SetPart>> {
+    let intersection = intersection_all(&inputs);
+
+    let mut result: Vec<Vec<model::SetPart>> = inputs
+        .iter()
+        .map(|input| unique_parts(input, &intersection))
+        .collect();
+
+    result.push(intersection.into_iter().collect());
+    result
+}
+
+
--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,9 @@
+use std::collections::{HashMap};
 use phf::phf_map;
+use serde::ser::{Serialize, Serializer, SerializeStruct};
+use crate::utils::convert_asset_url;
+use crate::csv::{InventoryPartRecord, InventoryMinifigRecord, MinifigRecord, PartRecord, SetRecord};
+use crate::csv::Data as CSVData;
 
 #[derive(Debug, serde::Serialize)]
 pub struct Minifig {
@@ -16,7 +21,7 @@ pub struct Part {
     pub material: String,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug)]
 pub struct Set {
     pub number: String,
     pub name: String,
@@ -24,11 +29,41 @@ pub struct Set {
     pub theme_id: u32,
     pub parts_count: u32, // ? relevant ?? doesn't vary per versions? TODO
     pub img_url: Option<String>,
-    pub versions: Vec<SetVersion>,
     pub is_pack: bool,
     pub is_unreleased: bool,
     pub is_accessories: bool,
+    pub versions: Vec<SetVersion>, 
 } 
+
+impl Set {
+    fn minifigs(&self) -> &Vec<SetMinifig> {
+        &self.versions.last().unwrap().minifigs
+    }
+    fn parts(&self) -> &Vec<SetPart> {
+        &self.versions.last().unwrap().parts
+    }
+}
+
+impl Serialize for Set {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where 
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Set", 2)?;
+        state.serialize_field("number", &self.number)?;
+        state.serialize_field("name", &self.name)?;
+        state.serialize_field("year", &self.year)?;
+        state.serialize_field("theme_id", &self.theme_id)?;
+        state.serialize_field("parts_count", &self.parts_count)?;
+        state.serialize_field("img_url", &self.img_url)?;
+        state.serialize_field("minifigs", &self.minifigs())?;
+        state.serialize_field("parts", &self.parts())?;
+        state.serialize_field("is_pack", &self.is_pack)?;
+        state.serialize_field("is_unreleased", &self.is_unreleased)?;
+        state.serialize_field("is_accessories", &self.is_accessories)?;
+        state.end()
+    }
+}
 
 #[derive(Debug, serde::Serialize)]
 pub struct SetVersion {
@@ -43,7 +78,7 @@ pub struct SetMinifig {
     pub quantity: u16,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize)]
 pub struct SetPart {
     pub number: String,
     pub color_id: u32,
@@ -57,6 +92,119 @@ pub struct Data {
     pub minifigs: Vec<Minifig>,
     pub parts: Vec<Part>,
     pub sets: Vec<Set>,
+}
+
+fn get_set_version(inventory_id: u32, version: u16, minifig_inventories: &HashMap<u32, Vec<InventoryMinifigRecord>>, part_inventories: &HashMap<u32, Vec<InventoryPartRecord>>, all_parts_keys: &HashMap<String, bool>) -> SetVersion {
+    let mut version = SetVersion {
+        version,
+        minifigs: vec![],
+        parts: vec![],
+    };
+
+    if let Some(minifigs) = minifig_inventories.get(&inventory_id) {
+        for minifig in minifigs {
+            let minifig = SetMinifig {
+                number: minifig.fig_num.clone(), // TODO no clone
+                quantity: minifig.quantity,
+            };
+            version.minifigs.push(minifig);
+        } 
+    }
+    if let Some(parts) = part_inventories.get(&inventory_id) {
+        for part in parts {
+            if all_parts_keys.contains_key(&part.part_num) {
+                let part = SetPart {
+                    number: part.part_num.clone(), // TODO no clone
+                    quantity: part.quantity,
+                    color_id: part.color_id.try_into().unwrap(),
+                    img_url: convert_asset_url(&part.img_url),
+                    is_spare: part.is_spare == "True",
+                };
+                version.parts.push(part);
+            } else {
+                eprintln!("Set Version {}: Ignoring part {}: does not exist", version.version, part.part_num);
+            }
+        }
+    }
+    version
+}
+
+pub fn convert(csv_data: CSVData) -> Box<Data> {
+    let mut parts: Vec<Part> = Vec::with_capacity(csv_data.parts.len());
+    let mut parts_map: HashMap<String, bool> = HashMap::new();
+    for part in csv_data.parts.into_iter() {
+        parts_map.insert(part.part_num.clone(), true);
+        parts.push(part_csv_to_model(part));
+    }
+    let mut minifigs: Vec<Minifig> = Vec::with_capacity(csv_data.minifigs.len());
+    for minifig in csv_data.minifigs.into_iter() {
+        minifigs.push(minifig_csv_to_model(minifig));
+    }
+    let mut set_inventories: HashMap<String, Vec<(u32, u16)>> = HashMap::new(); 
+    for inventory in csv_data.inventories.into_iter() {
+        let versions = set_inventories.entry(inventory.set_num.clone()).or_default();
+        versions.push((inventory.id, inventory.version));        
+    }
+    let mut part_inventories: HashMap<u32, Vec<InventoryPartRecord>> = HashMap::new();
+    for part in csv_data.inventories_parts.into_iter() {
+        let parts = part_inventories.entry(part.inventory_id).or_default(); 
+        parts.push(part);
+    }
+    let mut minifig_inventories: HashMap<u32, Vec<InventoryMinifigRecord>> = HashMap::new();
+    for minifig in csv_data.inventories_minifigs.into_iter() {
+        let minifigs = minifig_inventories.entry(minifig.inventory_id).or_default();
+        minifigs.push(minifig);
+    }
+    let mut sets: Vec<Set> = Vec::with_capacity(csv_data.sets.len());
+    for set in csv_data.sets.into_iter() {
+        let key = set.set_num.clone();
+        let mut set = set_csv_to_model(set, is_pack(&key), is_unreleased(&key), is_accessories(&key));
+        if let Some(versions) = set_inventories.get(&set.number) {
+            for version in versions {
+                let version = get_set_version(version.0, version.1, &minifig_inventories, &part_inventories, &parts_map);
+                set.versions.push(version);
+            }
+        }
+        sets.push(set);
+    }
+    Box::new(Data{
+        minifigs,
+        parts,
+        sets,
+    })
+}
+
+fn part_csv_to_model(part: PartRecord) -> Part {
+    Part {
+        number: part.part_num,
+        name: part.name,
+        part_category_id: part.part_cat_id,
+        material: part.part_material,
+    }
+}
+
+fn minifig_csv_to_model(minifig: MinifigRecord) -> Minifig {
+    Minifig {
+        number: minifig.fig_num,
+        name: minifig.name,
+        parts_count: minifig.num_parts,
+        img_url: convert_asset_url(&minifig.img_url),
+}
+}
+
+fn set_csv_to_model(set: SetRecord, is_pack: bool, is_unreleased: bool, is_accessories: bool) -> Set {
+    Set {
+        number: set.set_num,
+        name: set.name,
+        year: set.year,
+        parts_count: set.num_parts,
+        theme_id: set.theme_id,
+        img_url: convert_asset_url(&set.img_url),
+        versions: vec![],
+        is_pack,
+        is_unreleased,
+        is_accessories,
+    }
 }
 
 static PACKS: phf::Map<&'static str, ()> = phf_map! {
